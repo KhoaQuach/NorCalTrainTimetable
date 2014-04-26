9a94519b958e593007156ca633014d8d9baf0b3a
Khoa Quach <kaquach@hotmail.com>
2014-04-26 00:45:04 -0700

    Initial revision

---
+++ gen/com/khoa/quach/norcalcaltraintimetable/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.khoa.quach.norcalcaltraintimetable;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
